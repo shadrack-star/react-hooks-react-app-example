91e11d7017cb1b13af39c91d741f3f008dbfc7bd
shadrack-star <shaddkiplangat98@gmail.com>
2024-04-27 23:33:39 +0300

    testcomponent linked

---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+function TestComponent (){
+
+    return(
+        <div>
+            This is a new component!
+        </div>
+
+    );
+}
+export default TestComponent;
